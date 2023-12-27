1d91cb77e1c97a1cca1fcc02167c0a199addf845
subbel <pkumar96@asu.edu>
2023-12-27 00:41:43 -0700

    transfer

--- pages/views.py
+++ pages/views.py
@@ -7,16 +7,21 @@ def home_view(request, *args, **kwargs):
     return render(request, "home.html", {})
 
 def about_view(request, *args, **kwargs):
-    return HttpResponse("<h1> Hello World <h1>")
+    # return HttpResponse("<h1> Hello World <h1>")
+    return render(request, "about.html", {})
 
 def eboard_view(request, *args, **kwargs):
-    return HttpResponse("<h1> Hello World <h1>")
+    # return HttpResponse("<h1> Hello World <h1>")
+    return render(request, "eboard.html", {})
 
 def gallery_view(request, *args, **kwargs):
-    return HttpResponse("<h1> Hello World <h1>")
+    # return HttpResponse("<h1> Hello World <h1>")
+    return render(request, "gallery.html", {})
 
 def donate_view(request, *args, **kwargs):
-    return HttpResponse("<h1> Hello World <h1>")
+    # return HttpResponse("<h1> Hello World <h1>")
+    return render(request, "donate.html", {})
 
 def meetings_view(request, *args, **kwargs):
-    return HttpResponse("<h1> Hello World <h1>")
+    # return HttpResponse("<h1> Hello World <h1>")
+    return render(request, "meetings.html", {})
