0f377af608e16f2448519d8b4d8e4943823d90be
subbel <pkumar96@asu.edu>
2023-12-27 01:06:38 -0700

    deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn SASEASU.wsgi
--- SASEASU/settings.py
+++ SASEASU/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,3 +127,7 @@ STATICFILES_DIRS = (
   os.path.join(BASE_DIR, 'static/'),
   os.path.join(BASE_DIR, 'static/Images'),
 )
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
