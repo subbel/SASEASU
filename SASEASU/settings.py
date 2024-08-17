4a48b8e35d5f0c4788ba1c7e7841bc815ba319f1
subbel <pkumar96@asu.edu>
2024-08-17 15:41:17 -0500

    new models without integrated userbase

--- SASEASU/settings.py
+++ SASEASU/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2dauto3)x^zh!)(_&tsfbaqj@9dkhf*ag9&f*g$rc#w@z6cnaz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'saseasu-production.up.railway.app',
@@ -42,11 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'pages',
-    'meetings',
-    'membership',
+    'meetings.apps.MeetingsConfig',
 ]
 
-AUTH_USER_MODEL = "membership.Student"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -134,5 +132,3 @@ STATICFILES_DIRS = (
   os.path.join(BASE_DIR, 'static/'),
   os.path.join(BASE_DIR, 'static/Images'),
 )
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
