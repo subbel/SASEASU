e5624a9b680cf3db760a66c4b8d8cfa21f15a69a
subbel <pkumar96@asu.edu>
2025-02-18 16:10:24 -0700

    removal of pages, and addition of secret url

--- meetings/apps.py
+++ meetings/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.db.models import BigAutoField
 
 
 class MeetingsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'meetings'
